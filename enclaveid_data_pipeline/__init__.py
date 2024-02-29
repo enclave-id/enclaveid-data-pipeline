4d3e493154bf405feb9e6293581fea1851fd4f83
Muhammad Jarir Kanji <mjkanji@outlook.com>
2024-02-29 05:59:05 +0500

    Disable experimental warnings from Dagster.

--- enclaveid_data_pipeline/__init__.py
+++ enclaveid_data_pipeline/__init__.py
@@ -1,6 +1,10 @@
-from dagster import Definitions, load_assets_from_modules
+import warnings
+
+from dagster import Definitions, ExperimentalWarning, load_assets_from_modules
 
 from . import assets
+
+warnings.filterwarnings("ignore", category=ExperimentalWarning)
 
 all_assets = load_assets_from_modules([assets])
 
