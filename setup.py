341d808e17367a731be354330bca1033e6349843
Muhammad Jarir Kanji <mjkanji@outlook.com>
2024-02-29 05:11:14 +0500

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="enclaveid_data_pipeline",
+    packages=find_packages(exclude=["enclaveid_data_pipeline_tests"]),
+    install_requires=[
+        "dagster>=1.6.0,<1.7.0",
+        "dagster-cloud",
+        "pandas",
+        "numpy",
+        "openai",
+        "httpx",
+        "tqdm",
+    ],
+    extras_require={"dev": ["dagster-webserver", "pytest", "ipython"]},
+)
