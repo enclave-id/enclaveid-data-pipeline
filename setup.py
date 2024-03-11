6d9ce18bb456b9d434e083a51a69e841d654f798
Muhammad Jarir Kanji <mjkanji@outlook.com>
2024-03-11 17:12:39 +0500

    Add more dependencies.

--- setup.py
+++ setup.py
@@ -4,13 +4,24 @@ setup(
     name="enclaveid_data_pipeline",
     packages=find_packages(exclude=["enclaveid_data_pipeline_tests"]),
     install_requires=[
+        "adlfs",
         "dagster>=1.6.0,<1.7.0",
         "dagster-cloud",
+        "dagster-polars",
+        "polars",
         "pandas",
+        "pyarrow",
         "numpy",
         "openai",
         "httpx",
         "tqdm",
     ],
-    extras_require={"dev": ["dagster-webserver", "pytest", "ipython"]},
+    extras_require={
+        "dev": [
+            "dagster-webserver",
+            "pytest",
+            "ipython",
+            "ipykernel",
+        ]
+    },
 )
