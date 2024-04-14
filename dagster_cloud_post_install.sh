4c5096de9530cda1cb95c1005476b164238de8c4
Muhammad Jarir Kanji <mjkanji@outlook.com>
2024-04-14 19:05:11 +0000

    Avoid apt and pip caches to minimize space used during Docker build

--- dagster_cloud_post_install.sh
+++ dagster_cloud_post_install.sh
@@ -1,3 +1,3 @@
 # TODO: Decide if this is needed after a decision on infrastructure for GPU assets
 # (i.e., whether to use SkyPilot or a Hybrid deployment) is made.
-pip install --extra-index-url=https://pypi.nvidia.com cuml-cu12==24.2.*
+pip install --extra-index-url=https://pypi.nvidia.com --no-cache-dir cuml-cu12==24.2.*
